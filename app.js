1f566402f7603cc8af0129ff5072b732ba0fc738
Esdras Ferreira <esdras.ferreira@aluno.uepb.edu.br>
2025-03-21 11:25:23 -0300

    adding a rout

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(routes);
+
 const logger = require("./logger/logger.js");
 
 
--- routes.js
+++ routes.js
@@ -8,8 +8,28 @@ const generator = require('generate-password');
 const bcrypt = require('bcrypt');
 
 router.get('/api/users', async (req, res)=>{
+
+    let users;
     
+    await mongo().then(async ()=>{
+        try{
+            const result = await userSchema.find();
+
+            users = result; 
+        
+        }catch(e){
+            logger.error(`Error when trying to find users, error: ${e}` );
+            res.status(500).json({msg: "Error when trying to find users, error: " + e})
+        }
+    });
+    
+    if (!users){
+        res.status(501).json({msg: "Users don't exist"});
+    }else{
+        res.status(200).json({users});
+    }
 })
+
 
 router.get('/api/users/:key', async (req, res)=>{
  
